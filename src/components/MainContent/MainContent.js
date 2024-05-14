01c68ca7b2c19900fb91730ff8e1244775d975a0
tarun <tarun.gautam@ultivic.com>
2024-05-14 11:56:34 +0530

    updates

--- src/components/MainContent/MainContent.js
+++ src/components/MainContent/MainContent.js
@@ -2,7 +2,7 @@ import React from "react";
 import MainLayout from "./MainLayout";
 import MainBase from "./MainBase";
 import MainParagraph from "./MainParagraph";
-// import MainParagraphTwo from "./MainPAragraphTwo";
+import MainParagraphTwo from "./MainPAragraphTwo";
 import MainReady from "./MainReady";
 import MainReadyImage from "./MainReadyContent";
 import MainEncAuto from "./MainEncAuto";
